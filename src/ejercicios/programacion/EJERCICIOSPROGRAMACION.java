17ba04fbf507424d5f1a1f0e55af25ee7bbc3fd2
Marioasa42 <asafteimariorobert@gmail.com>
2025-02-05 10:09:54 +0100

    Cambio de prueba xd

--- src/ejercicios/programacion/EJERCICIOSPROGRAMACION.java
+++ src/ejercicios/programacion/EJERCICIOSPROGRAMACION.java
@@ -3,17 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package ejercicios.programacion;
-import java.util.Scanner;
 public class EJERCICIOSPROGRAMACION {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Primera frase: "); 
-        String frase1 = sc.nextLine();
-        System.out.println("Segunda frase: "); 
-        String frase2 = sc.nextLine();
-        if (frase1.length() < frase2.length()) System.out.println("La frase 2 es mas larga que la frase 1");
-        else if(frase1.length() < frase2.length()) System.out.println("La frase 1 es mas larga que la frase 2");
-        else System.out.println("La frase 2 es igual de larga que la frase 1");
+        System.out.println("Prueba del git de los hub");
     }
     
 }
